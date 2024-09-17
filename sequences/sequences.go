3887bb88e8a0e352fedc0e05e26d59ad24285809
emmaegnot <bb23833@bristol.ac.uk>
2024-09-17 21:20:48 +0100

    question 3b

--- sequences/sequences.go
+++ sequences/sequences.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func addOne(a int) int {
 	return a + 1
 }
@@ -12,14 +14,24 @@ func double(slice []int) {
 	slice = append(slice, slice...)
 }
 
-func mapSlice(f func(a int) int, slice []int) {
-
+func mapSlice(f func(a int) int, slice []int) []int {
+	for i := 0; i < len(slice); i++ {
+		slice[i] = f(slice[i])
+	}
+	return slice
 }
 
-func mapArray(f func(a int) int, array [3]int) {
-
+func mapArray(f func(a int) int, array [3]int) [3]int {
+	for i := 0; i < len(array); i++ {
+		array[i] = f(array[i])
+		fmt.Println()
+	}
+	return array
 }
 
 func main() {
-
+	var mySlice = []int{1, 2, 3}
+	fmt.Println(mapSlice(addOne, mySlice))
+	var myArray = [3]int{1, 2, 3}
+	fmt.Println(mapArray(addOne, myArray))
 }
